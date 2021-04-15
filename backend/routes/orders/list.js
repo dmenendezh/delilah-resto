fb2c850995513f615240a0b74830d1a0c7743eda
dmenendezh <diego.andres.menendez@gmail.com>
2021-04-15 12:11:12 -0300

    Sync list order admin or common user control

--- backend/routes/orders/list.js
+++ backend/routes/orders/list.js
@@ -5,14 +5,30 @@ const mdGlobal = require('../../middlewares/mdGlobal');
 const mdUsers = require('../../middlewares/mdUsers');
 
 router.get('/', mdGlobal.validateToken, async (req, res) => {
-    const orders = await Orders.ordersModel.findAll({})
-    .catch(err => throwException(err, res));
+  
+    if(res.locals.userLogged.admin === 1){//list all orders if is an admin user
+        const orders = await Orders.ordersModel.findAll({})
+        .catch(err => throwException(err, res));
+    
+        res.status(200).json({
+            message: 'Returning all orders:',
+            quantity: orders.length,
+            orders
+        });
+    }else{//list user order 
 
-    res.status(200).json({
-        message: 'Returning all available orders.',
-        quantity: orders.length,
-        orders
-    });
+        const user_id = res.locals.userLogged.usr_id;
+
+        const orders = await Orders.ordersModel.findAll({
+            where: {order_user: user_id}
+        }).catch(err => throwException(err, res));
+
+        res.status(200).json({
+            message: 'User orders:',
+            quantity: orders.length,
+            orders
+        });
+    }    
 });
 
 const throwException = (err, res) => {
